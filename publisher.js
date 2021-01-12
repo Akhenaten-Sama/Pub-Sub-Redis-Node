22f729b245ebcf3ed19bfecdf4dc46b6369e8c55
Efunkunle Olalekan <efunkunleolalekan@gmail.com>
2021-01-12 14:35:31 +0100

    initial commit

---
+++ publisher.js
@@ -0,0 +1,86 @@
+const fetch = require('axios');
+const express = require('express');
+const redis = require('redis');
+const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config()
+
+
+
+const CONFIG = {
+  url:process.env.URL,
+  host:process.env.HOST,
+  password:process.env.PASSWORD
+}
+const publisher = redis.createClient(CONFIG);
+
+
+const subscriber = redis.createClient(CONFIG);
+
+
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+
+var subscribers = []
+app.post('/publish/:topic',(req,res) => {
+    const message = req.body;
+    const topic = req.params.topic;
+       console.log(message)
+    try{
+      publisher.publish(topic,JSON.stringify(message))
+      res.status(200).send("Message has been published");
+    }catch(err){
+         res.status(501).send(err)
+    }
+    
+})
+
+
+
+app.post('/subscribe/:topic', (req, res) => {
+	var topic = req.params.topic;
+	var url = req.body.url;
+console.log('request received')
+subscribers.push(url)
+
+
+	try {
+		subscriber.subscribe(topic);
+		res.status(200).send({ url, topic });
+	} catch (err) {
+		res.status(501).send('unable to subscribe');
+	}
+	console.log(topic);
+});
+
+
+subscriber.on('message', (topic, data) => {
+  console.log({ topic, data });
+  subscribers.map(sub=>{
+    fetch({
+      method:'post',
+      url:sub,
+      data:{
+        topic:topic,
+        data:data
+      },
+      header:{'Content-Type':'application/x-www-form-urlencoded'}
+    
+    }).then(result => console.log('sent'))
+    .catch(err=> console.log('error'))
+  })
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT,() => {
+    console.log(`server is listening on PORT ${process.env.PORT}`);
+})
---
+++ readme.md
@@ -0,0 +1,8 @@
+This is my answer for the Pangaea Takehome.
+git re
+
+nmp run start =>> starts the Publisher server
+
+npm run sub =>> starts the Subscribers' server
+
+All instructions were followed and the app works as expected
